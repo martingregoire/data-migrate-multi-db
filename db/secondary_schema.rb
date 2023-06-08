f8ab7207ab051d24859ef22211e4f09c0402db88
Martin Gregoire <mgre@honeypot.io>
2023-06-08 09:47:44 +0200

    Add Comment model to secondary database
    ```
    rails g model Comment body:string --database secondary
    rails db:migrate
    ```

--- db/secondary_schema.rb
+++ db/secondary_schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_074631) do
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
